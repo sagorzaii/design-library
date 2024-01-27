c3853f2a1e21abd7539389b2aa4e6417bccd1805
Tian Sheng <siewtiansheng@gmail.com>
2024-01-27 21:37:38 +0800

    update sb config

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -8,26 +8,26 @@ const config: StorybookConfig = {
     "@storybook/addon-onboarding",
     "@storybook/addon-interactions",
   ],
-  webpackFinal: async (config, { configType }) => {
-    // Add any custom webpack configurations here
+  // webpackFinal: async (config, { configType }) => {
+  //   // Add any custom webpack configurations here
 
-    // Add Babel loader for JavaScript and JSX files
-    (config.module || (config.module = {})).rules?.push({
-      test: /\.(js|jsx|ts|tsx)$/,
-      use: [
-        {
-          loader: require.resolve("babel-loader"),
-          options: {
-            presets: ["@babel/preset-react", "@babel/preset-env"],
-          },
-        },
-      ],
-    });
+  //   // Add Babel loader for JavaScript and JSX files
+  //   (config.module || (config.module = {})).rules?.push({
+  //     test: /\.(js|jsx|ts|tsx)$/,
+  //     use: [
+  //       {
+  //         loader: require.resolve("babel-loader"),
+  //         options: {
+  //           presets: ["@babel/preset-react", "@babel/preset-env"],
+  //         },
+  //       },
+  //     ],
+  //   });
 
-    // Add any additional loaders, plugins, or modifications to the configuration
+  //   // Add any additional loaders, plugins, or modifications to the configuration
 
-    return config;
-  },
+  //   return config;
+  // },
   framework: {
     name: "@storybook/react-webpack5",
     options: {
