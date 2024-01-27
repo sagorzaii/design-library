4cb815b9b6e9743a65696d6d6fadd175fc5fb225
Tian Sheng <siewtiansheng@gmail.com>
2024-01-27 20:42:09 +0800

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,49 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  mode: "development",
+  entry: {
+    index: "./src/index.ts",
+  },
+  devtool: "inline-source-map",
+  devServer: {
+    static: "./dist",
+    compress: true,
+    port: 8080,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "Design Library",
+    }),
+  ],
+  optimization: {
+    runtimeChunk: "single",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ["babel-loader"],
+      },
+      {
+        test: /\.tsx?$/,
+        use: "ts-loader",
+        exclude: "/node_modules/",
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
+  },
+  output: {
+    filename: "[name].bundle.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+  },
+};
